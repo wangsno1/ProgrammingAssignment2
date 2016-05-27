58ffb08b3b49977706ec1b520220a86c9ff6def5
wangsno1 <snowyouwang@gmail.com>
2016-05-28 00:09:55 +0800

    Added assignment code
    Completed assignment code by adding to file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R Programming Week 3 Assignment
+# Write 2 functions: first which creates an object which can cache matrices and their inverses
+# second which can evaluate if the matrix is the same or not and whether to pull the cached inverse
 
-## Write a short comment describing this function
+# Matrix Equality Code: Credit to https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html
+matequal <- function(x, y)
+    is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
+
+## Create Cache Matrix class with the ability to save the inverse in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL # Set inv as null to initialize
+    set <- function(y) { # Set method which sets new matrix to cache
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x # Get the matrix
+    setinv <- function(invs) inv <<- invs # Set the presented inverse in cache
+    getinv <- function() inv # Obtain cached inverse
+    list(set = set, get = get, #Create object
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Compares if the new matrix is the same as the old matrix x
+# if so then see if the inverse is in cache, if no, calculate inverse
+# if it is in the cache, pull it from cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(newmat, x, ...){
+    origmat <- x$get()
+    inv <- x$getinv()
+    if (matequal(newmat,origmat) & !is.null(inv)) {
+        # Pull From Cache since it exists
+        message("getting cached data")
+        return (x$getinv())
+    } else if (matequal(newmat,origmat) & is.null(inv)) {
+        # Calculate inverse since it does not exist
+        message("Calculating inverse")
+        invers <- solve(origmat)
+        x$setinv(invers)
+        return (invers)
+    } else {
+        # Calculate inverse since the matrix is different from before
+        message("Calculating inverse")
+        x$set(newmat)
+        invers <- solve(newmat, ...)
+        x$setinv(invers)
+        return (invers)
+    }
 }
